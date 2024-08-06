48afb6e49b9fd5ec100c066d0994bb8e64fd90f6
vishal20061 <156406242+vishal20061@users.noreply.github.com>
2024-08-06 09:37:17 +0530

    Add files via upload

---
+++ color-picker/color-picker.js
@@ -0,0 +1,38 @@
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+
+btn1.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "red"
+};
+btn2.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "blue"
+};
+btn3.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "green"
+};
+btn4.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "black"
+};
+btn5.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "gray"
+};
+btn6.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "yellow"
+};
+btn7.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "pink"
+};
+btn8.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "orange"
+};
+btn9.onclick = () =>{
+    document.querySelector("body").style.backgroundColor = "brown"
+};
+
